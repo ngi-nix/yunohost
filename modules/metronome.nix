abb34cb5a4dd4b3b5665f2d01dd6fb03b9ddbfd5
Edmund Wu <fangkazuto@gmail.com>
2020-07-08 15:11:03 -0400

    modules/metronome: init

---
+++ modules/metronome.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.services.metronome;
+in
+{
+  options.services.metronome = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Enable Metronome IM
+      '';
+    };
+
+    configFile = mkOption {
+      type = types.path;
+      default = pkgs.metronome + "/etc/metronome/metronome.cfg.lua";
+      description = ''
+        Metronome IM configuration file
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.etc = {
+      # or symlinkJoin config file with this...
+      # "metronome".source = pkgs.metronome + "/etc/metronome";
+
+      "metronome/metronome.cfg.lua".source = cfg.configFile;
+      "metronome/certs".source = pkgs.metronome + "/etc/metronome/certs";
+      "metronome/templates".source = pkgs.metronome + "/etc/metronome/templates";
+    };
+
+    users = {
+      users.metronome = {
+        group = "metronome";
+        description = "Metronome IM XMPP Server";
+        home = "/var/lib/metronome";
+      };
+
+      groups.metronome = {};
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /var/lib/metronome 0755 metronome metronome"
+      "d /var/log/metronome 0700 metronome metronome"
+    ];
+
+    systemd.services.metronome = {
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      path = with pkgs; [ openssl metronome.lua ];
+      description = "Instant Messaging (XMPP) Server";
+
+      serviceConfig = {
+        Type = "forking";
+        User = "metronome";
+        Group = "metronome";
+        ExecStart = "${pkgs.metronome}/bin/metronome start";
+        ExecStop = "${pkgs.metronome}/bin/metronome stop";
+
+        PermissionsStartOnly = true;
+        RuntimeDirectory = "metronome";
+        RuntimeDirectoryMode = "0750";
+
+        PIDFile = "/run/metronome/metronome.pid";
+      };
+    };
+  };
+}
